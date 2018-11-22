fa3dff79a4a41ddedf64cf744029d0e4b1e69156
aereal <aereal@aereal.org>
2018-11-22 12:21:05 +0900

    Pass upstream

--- consumer_app/main.go
+++ consumer_app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
@@ -8,8 +9,15 @@ import (
 	"github.com/aereal/demo-mechakucha/webbase"
 )
 
+var (
+	UPSTREAM_ORIGIN string
+)
+
+func init() {
+	UPSTREAM_ORIGIN = os.Getenv("UPSTREAM_ORIGIN")
+}
+
 func main() {
-	// TODO: upstream origin
 	config := &webbase.Config{HostPort: ":8001"}
 	if err := webbase.Run(config, handler()); err != nil {
 		fmt.Fprintf(os.Stderr, "%+v\n", err)
@@ -17,11 +25,16 @@ func main() {
 	}
 }
 
+type rootResponse struct {
+	Upstream string
+}
+
 func handler() http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("content-type", "application/json")
-		fmt.Fprintln(w, `{"ok":true}`)
+		res := &rootResponse{Upstream: UPSTREAM_ORIGIN}
+		json.NewEncoder(w).Encode(&res)
 	})
 	return mux
 }
